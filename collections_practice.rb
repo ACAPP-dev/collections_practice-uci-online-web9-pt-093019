3a992024e08913e772e73112224cebede8bab812
Andrew Capp <acapp909@gmail.com>
2019-10-15 02:18:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,11 @@ end
 def sort_array_char_count(array)
   array.sort {|a,b| a.length <=> b.length}
 end
+
+def swap_elements(array)
+  
+  array[1] , array[2] = array[2] , array [1]
+  array
+end
+
+  
